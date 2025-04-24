c307c1e3a50a6cf6b9c9a89a6e3f49504060eff4
dornascarol <carolinafdornas@gmail.com>
2025-04-23 23:20:16 -0300

    GORM and data migration

--- models/singer.go
+++ models/singer.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type Singer struct {
+	gorm.Model
 	ArtistName   string `json:"artist_name"`
 	SongName     string `json:"song_name"`
 	MusicalGenre string `json:"musical_genre"`
