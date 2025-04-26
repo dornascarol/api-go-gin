4e078ae86a5c713ff1af6354c5dc628bd14ab9b4
dornascarol <carolinafdornas@gmail.com>
2025-04-26 10:48:51 -0300

    singer not found

--- controllers/controller.go
+++ controllers/controller.go
@@ -36,5 +36,12 @@ func SearchSingerById(c *gin.Context) {
 	var singer models.Singer
 	id := c.Params.ByName("id")
 	database.DB.First(&singer, id)
+
+	if singer.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Singer not found"})
+		return
+	}
+
 	c.JSON(http.StatusOK, singer)
 }
