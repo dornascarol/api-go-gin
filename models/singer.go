03a5938e48bcc518a646427ada8fae9cc25eb7fd
dornascarol <carolinafdornas@gmail.com>
2025-05-06 19:58:27 -0300

    create validations in struct

--- models/singer.go
+++ models/singer.go
@@ -1,10 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Singer struct {
 	gorm.Model
-	ArtistName   string `json:"artist_name"`
-	SongName     string `json:"song_name"`
-	MusicalGenre string `json:"musical_genre"`
+	ArtistName   string `json:"artist_name" validate:"nonzero"`
+	SongName     string `json:"song_name" validate:"len=50"`
+	MusicalGenre string `json:"musical_genre" validate:"nonzero"`
+}
+
+func ValidateSingerData(singer *Singer) error {
+	if err := validator.Validate(singer); err != nil {
+		return err
+	}
+	return nil
 }
