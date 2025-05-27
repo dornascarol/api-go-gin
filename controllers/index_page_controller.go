52ae55c1913121bd05c121c4b027652abaa46125
dornascarol <carolinafdornas@gmail.com>
2025-05-27 09:54:13 -0300

    displaying singers in the index

--- controllers/index_page_controller.go
+++ controllers/index_page_controller.go
@@ -3,11 +3,15 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/dornascarol/api-go-gin/database"
+	"github.com/dornascarol/api-go-gin/models"
 	"github.com/gin-gonic/gin"
 )
 
 func DisplayIndexPage(c *gin.Context) {
+	var singers []models.Singer
+	database.DB.Find(&singers)
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"message": "Welcome",
+		"singers": singers,
 	})
 }
